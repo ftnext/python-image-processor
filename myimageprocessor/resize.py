15bdf1fbbc25719d5cb6e2eb43c7ede859dd38e0
ftnext <takuyafjp+develop@gmail.com>
2019-11-30 12:25:52 +0900

    [feat] テストを通すようにresize_imageの実装をアップデート
    * テストを通す実装はもっと簡単にできた（知っているがゆえの実装になっていた）

--- myimageprocessor/resize.py
+++ myimageprocessor/resize.py
@@ -17,3 +17,6 @@ class ResizeCalculator:
             new_width = self._limit
             new_height = int((self._limit / width) * height)
             return (new_width, new_height)
+        new_width = int((self._limit / height) * width)
+        new_height = self._limit
+        return (new_width, new_height)
