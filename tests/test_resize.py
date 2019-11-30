a5604636e60d9ce25fd152d50f28fd2183b83c11
ftnext <takuyafjp+develop@gmail.com>
2019-11-30 12:08:37 +0900

    [docs] add explanation of test case

--- tests/test_resize.py
+++ tests/test_resize.py
@@ -14,24 +14,28 @@ class ResizeCalculatorTestCase(TestCase):
         self.assertEqual(actual._limit, 300)
 
     def test_needs_resize(self):
+        """Case of width > limit and height > limit"""
         size = (600, 400)
         resize_calculator = r.ResizeCalculator(size, self.limit)
         actual = resize_calculator.needs_resize()
         self.assertTrue(actual)
 
     def test_needs_resize_width_smaller(self):
+        """Case of width < limit (< height)"""
         size = (299, 500)
         resize_calculator = r.ResizeCalculator(size, self.limit)
         actual = resize_calculator.needs_resize()
         self.assertFalse(actual)
 
     def test_needs_resize_height_smaller(self):
+        """Case of height < limit (< width)"""
         size = (400, 250)
         resize_calculator = r.ResizeCalculator(size, self.limit)
         actual = resize_calculator.needs_resize()
         self.assertFalse(actual)
 
     def test_needs_resize_boundary(self):
+        """Case of height == limit < width"""
         size = (301, 300)
         resize_calculator = r.ResizeCalculator(size, self.limit)
         actual = resize_calculator.needs_resize()
