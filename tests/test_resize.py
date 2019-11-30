bd4f4d02d0fbf139b0f5999202f7bcf2fb24033c
ftnext <takuyafjp+develop@gmail.com>
2019-11-30 11:38:29 +0900

    [test] 縮小する必要があるか判断するメソッドneeds_resizeのテストを追加（縮小してほしいケース）

--- tests/test_resize.py
+++ tests/test_resize.py
@@ -4,9 +4,17 @@ import myimageprocessor.resize as r
 
 
 class ResizeCalculatorTestCase(TestCase):
+    def setUp(self):
+        self.limit = 300
+
     def test_init(self):
         size = (400, 600)
-        limit = 300
-        actual = r.ResizeCalculator(size, limit)
+        actual = r.ResizeCalculator(size, self.limit)
         self.assertEqual(actual._now, (400, 600))
         self.assertEqual(actual._limit, 300)
+
+    def test_needs_resize(self):
+        size = (600, 400)
+        resize_calculator = r.ResizeCalculator(size, self.limit)
+        actual = resize_calculator.needs_resize()
+        self.assertTrue(actual)
