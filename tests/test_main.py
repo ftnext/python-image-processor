e03651b464c9772a5190db7b6aadbb2a74c18d4c
ftnext <takuyafjp+develop@gmail.com>
2020-02-01 16:41:14 +0900

    [refactor] targetのリストをprocessに渡して処理する変更を適用

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import call, MagicMock, patch
 
 from myimageprocessor import main
-import myimageprocessor.path_handler as ph
 
 
 class MainTestCase(TestCase):
@@ -11,10 +10,9 @@ class MainTestCase(TestCase):
     @patch("myimageprocessor.parser.parse_args")
     def test(self, parse_args, create_path_pair, create_shrink_processor):
         shrink_size = MagicMock(spec=int)
-        pair = MagicMock(spec=ph.SourceDestinationPair)
         args = parse_args.return_value
         path_pair = create_path_pair.return_value
-        path_pair.list_targets.return_value = [pair]
+        targets = path_pair.list_targets.return_value
         processor = create_shrink_processor.return_value
 
         main(shrink_size)
@@ -26,6 +24,6 @@ class MainTestCase(TestCase):
         )
         self.assertEqual(path_pair.list_targets.call_args_list, [call()])
         self.assertEqual(
-            create_shrink_processor.call_args_list, [call(pair, shrink_size)],
+            create_shrink_processor.call_args_list, [call(shrink_size)],
         )
-        self.assertEqual(processor.process.call_args_list, [call()])
+        self.assertEqual(processor.process.call_args_list, [call(targets)])
