9ffdb28c25accc9667437f484e5c9b10a1eba80b
ftnext <takuyafjp+develop@gmail.com>
2020-02-05 00:54:42 +0900

    [feat] sourceのみ指定した時、destinationをカレントディレクトリとする挙動に変更（WIP: no test）

--- myimageprocessor/__init__.py
+++ myimageprocessor/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import myimageprocessor.parser as p
 import myimageprocessor.path_handler as ph
 import myimageprocessor.resize as r
@@ -7,7 +9,8 @@ SHRINK_SIZE = 300
 
 def main(shrink_size=SHRINK_SIZE):
     args = p.parse_args()
-    path_pair = ph.create_path_pair(args.source)
+    destination = Path.cwd()
+    path_pair = ph.create_path_pair(args.source, destination)
     targets = path_pair.list_targets()
     processor = r.create_shrink_processor(shrink_size)
     processor.process(targets)
